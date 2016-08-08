50298544680bc8e8bad5e9ccdffde72df8f32fcf
salinasays <sfu.concepts@gmail.com>
2016-08-08 14:07:29 -0400

    Update main.js

--- week03/js/main.js
+++ week03/js/main.js
@@ -2,69 +2,84 @@ $(document).ready(function() {
 
     // Step 1: Dynamic resize
     $(window).resize(function(){
-    	if $(window.innerwidth > 500) {
+    	if ($(window).innerWidth() > 500) {
     		$("#frame").removeClass("small").addClass("big")
     	} else {
     		$("#frame").removeClass("big").addClass("small")
     	}
     })
     	
-}
+})
     
 
-    // Step 2: Checkbox genre filters
-    // $("input:checkbox").onclick =function(){
-    // 	if ($("#animation").prop('checked') == true) {
-    // 			$("#animation").show()
-    // 	} else {
-    // 		$("#animation").hide()
-    // 	}
+    //Step 2: Checkbox genre filters
+ //    $("input:checkbox").change(function(){
+ //    	if ($("#animation").prop('checked')) {
+ //    			$("#animation").show()
+ //    	} else {
+ //    		$("#animation").hide()
+ //    	}
+ //    })
     
 
-    // $("input:checkbox").onclick =function(){
-    // 	if ($("#action").prop('checked') == true) {
-    // 			$("#action").show()
-    // 	} else {
-    // 		$("#action").hide()
-    // 	}
-    // }
+ //    $("input:checkbox").change(function(){
+ //    	if ($("#action").prop('checked')) {
+ //    			$("#action").show()
+ //    	} else {
+ //    		$("#action").hide()
+ //    	}
+ //    })
 
-    // $("input:checkbox").onclick =function(){
-    // 	if ($("#drama").prop('checked') == true) {
-    // 			$("#drama").show()
-    // 	} else {
-    // 		$("#drama").hide()
-    // 	}
-    // }
+ //    $("input:checkbox").change(function(){
+ //    	if ($("#drama").prop('checked')) {
+ //    			$().show()
+ //    	} else {
+ //    		$("#drama").hide()
+ //    	}
+ //    })
     
 
-    // Step 3: Text box year filter
+ //    // Step 3: Text box year filter
     $('form').submit(function (event) { event.preventDefault(); })
 
-  //   $("#year").keyup(function(){
-  //   	var userYear = parseInt($("#year").val())
+ //    $("#year").keyup(function(){
+ //    	var userYear = parseInt($("#year").val());
 
-  //   	$('div.movie').each(function () {
+ //    	$('div.movie').each(function () {
 
-  //     	var movieYear = parseInt( $(this).find('dd.year').html() )
+ //      	var movieYear = parseInt( $(this).find('dd.year').html() )
 
-  //     	if(userYear < movieYear) {
-  //     		$(this).hide()
-  //     	} else {
-  //     		$(this).show()
-  //     	}
+ //      	if(userYear < movieYear) {
+ //      		$(this).hide()
+ //      	} else {
+ //      		$(this).show()
+ //      	}
 
-  // })
+ //  	})
 
-  //   })
+ // });
 
 
-    // Step 4: Both filters together
+//     Step 4: Both filters together
   	$('input').bind('keyup change', function (event) {
   		var userYear = parseInt($("input:year").val())
 
- 	})
+  		if (isNaN(userYear)) {
+    		userYear = 0
+  		}
 
+  		$('div.movie').each(function () {
 
-});
+      	var movieYear = parseInt($(this).find('dd.year').html())
+      	var movieGenre = parseInt($(this).find('dd.genre').html())
+      	var checked = $('input#'+movieGenre).prop('checked')
 
+      	if(movieYear > userYear && checked == true) {
+      		$(this).show()
+      	} else {
+      		$(this).hide()
+      	}
+
+  	})
+
+ 	});
